03f3866c50e58010a62b5bce4ec4cb9bf5f9096d
SayPoj <KorshikovPA1@gmail.com>
2021-04-18 07:55:32 +0300

    101

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, TemplateRef, ViewChild} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import get from 'get-js';
+import { NavigationEnd, Router } from "@angular/router";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,164 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TestVkAuth';
+  @ViewChild('VKModal') public VKModal: TemplateRef<any>;
+  user_id;             // id пользователя VK
+  user_access_token;   // Ключ доступа пользователя VK
+  client_id = 7826370; // Идентификатор приложения VK
+  authUrl = 'https://oauth.vk.com/authorize?client_id=' + this.client_id +
+    '&redirect_uri=' + window.location.origin + '' +
+    '&scope=photos&response_type=token&v=5.130' +
+    '&revoke='+this.isReAuthorization()
+    +'&state=now'
+  image
+  masks = [
+    {path:'../assets/1.png'},
+    {path:'../assets/2.png'},
+    {path:'../assets/3.png'},
+    {path:'../assets/4.png'},
+    {path:'../assets/5.png'},
+    {path:'../assets/6.png'},
+    {path:'../assets/7.png'},
+    {path:'../assets/8.png'},
+    {path:'../assets/9.png'},
+    {path:'../assets/10.png'},
+    {path:'../assets/11.png'},
+    {path:'../assets/12.png'}
+  ]
+  activeMasks = []
+  masksIsCollapsed = true;
+  settingsIsCollapsed = true;
+  VKUserPhoto; // Аватарка пользователя
+  VKPhotos: any[] = []
+  isJQuery = false
+  isReAuthorization(){
+    return localStorage.getItem('re-authorization')?0:1
+  }
+
+  constructor(private http: HttpClient,
+              private modalService: NgbModal,
+              private router: Router,
+              public changeDetector: ChangeDetectorRef) {
+    get('https://code.jquery.com/jquery-3.6.0.min.js')
+    get('https://code.jquery.com/ui/1.12.0/jquery-ui.min.js').then(()=>{
+      this.isJQuery = true
+      // @ts-ignore
+      $('.draggableHelper').draggable()
+      // @ts-ignore
+      $('.image').resizable()
+    })
+    router.events.subscribe(s => { // парсинг url
+      if (s instanceof NavigationEnd) {
+        let params = new URLSearchParams(s.url.split('#')[1]);
+        this.user_access_token = params.get('access_token');
+        this.user_id = +params.get('user_id');
+        let state = params.get('state');
+
+        if( state == 'now') {
+          localStorage.clear()
+          localStorage.setItem('user_id', this.user_id)
+          localStorage.setItem('user_access_token', this.user_access_token)
+          localStorage.setItem('access_token_expires_to', Date.now() + params.get('expires_in'))
+          this.router.navigate(['/'] );
+          this.openVKModal(this.VKModal)
+          return;
+        }
+
+        if((!this.user_id || !this.user_access_token ) && localStorage.getItem('user_access_token') && localStorage.getItem('user_access_token')) {
+          if(+localStorage.getItem('access_token_expires_to')<=Date.now()){
+            localStorage.clear()
+            localStorage.setItem('re-authorization', '1')
+          }
+          this.user_access_token = localStorage.getItem('user_access_token')
+          this.user_id = localStorage.getItem('user_id')
+        }
+      }
+    })
+  }
+
+  getAllPhotos() {
+    // @ts-ignore
+    if(this.isJQuery) {
+      // @ts-ignore
+      $.ajax({
+        url: 'https://api.vk.com/method/photos.getAll'
+          + '?access_token=' + this.user_access_token
+          + '&v=5.130'
+          + '&owner_id=' + this.user_id
+          + '&photo_sizes=1'
+        ,
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        success: data => {
+          this.VKPhotos = []
+          for (let photo of data.response.items) {
+
+
+            let maxHeight = Math.max(...photo.sizes.map(o => o.height), 0)
+
+            this.VKPhotos.push(photo.sizes.filter(photo => photo.height == maxHeight)[0].url)
+          }
+        }
+      })
+      this.getAva()
+    } else {
+      setTimeout(()=>{
+        this.getAllPhotos()
+      },50)
+      return
+    }
+  }
+
+  fileBrowseHandler(files) {
+    if (FileReader && files && files.length) {
+      var fr = new FileReader();
+      fr.onload =  () => {
+        // @ts-ignore
+        this.image = fr.result;
+      }
+      fr.readAsDataURL(files[0]);
+    }
+  }
+
+  openVKModal(content) {
+    this.modalService.open(content, { centered: true, size: 'xl' });
+    this.getAllPhotos()
+  }
+
+  closeVKModal() {
+    this.modalService.dismissAll()
+  }
+
+  getAva() {
+    // @ts-ignore
+    $.ajax({
+      url: 'https://api.vk.com/method/users.get'
+        + '?access_token=' + this.user_access_token
+        + '&v=5.130'
+        + '&user_ids=' + this.user_id
+        + '&fields=photo_max_orig',
+      type: 'GET',
+      dataType: 'jsonp',
+      crossDomain: true,
+      success: data => {
+        this.VKUserPhoto = data.response[0].photo_max_orig
+        /*if(data.response[0].photo_max_orig){
+          // @ts-ignore
+          document.getElementById('image').src = data.response[0].photo_max_orig
+        }*/
+      }
+    })
+  }
+
+  addActiveMask(data) {
+    this.activeMasks.push(data);
+    this.changeDetector.detectChanges()
+    // @ts-ignore
+    $('.draggableHelper').draggable()
+    // @ts-ignore
+    $('.image').resizable()
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http';
+import { DndDirective } from './dnd.directive';
+import {NgbCollapseModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DndDirective
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        NgbCollapseModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
